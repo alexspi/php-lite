912da323e08cde7682a0dc8896f795aed53a1bf4
alexspi <alexspi1@yandex.ru>
2021-10-17 16:16:24 +0300

    lesson3

---
+++ src/index.php
@@ -0,0 +1,43 @@
+<?php
+$menu = [
+    'Главная',
+    'Новости' => [
+        'Новости о спорте',
+        'Новости о политике',
+        'Новости о мире'
+    ],
+    'Контакты',
+    'Справка'
+];
+
+$str = '';
+
+foreach ($menu as $key => $links) {
+    if(is_array($links)) {
+        $str .= "<div><a><span>{$key}</span></a><div>";
+        foreach ($links as $link) {
+                  $str .= "<a>{$link}</a>";
+        }
+        $str .= '</div></div>';
+    } else {
+        $str .= "<div><a><span>{$links}</span></a></div>";
+    }
+}
+?>
+
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-5">
+    <link href=style.css rel=stylesheet>
+</head>
+<body>
+<header>
+	<nav>
+        <?php
+            echo $str;
+        ?>
+	</nav>
+</header>
+<h1>Заголовок</h1>
+<p>Информация</p>
+
+</body></html>
